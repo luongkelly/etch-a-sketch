1975f2465d8f098527f955a051db76d447cff8c9
Kelly Luong <kellyluong06@gmail.com>
2024-01-02 19:32:50 -0600

    Added clear function and allowed the grid to be different size

--- etch.js
+++ etch.js
@@ -11,30 +11,83 @@ let currentColor = '#333333';
 let currentSize = 16;
 let eraserColor = '#FFFFFF';
 let eraserMode = false;
+let rainbowMode = false;
 
 function setColor(newColor) {
     currentColor = newColor;
+    eraserMode = false;
+    eraser.classList.remove('active');
+    colorBtn.classList.add('active');
+}
+
+function setSize(newSize) {
+    currentSize = newSize;
 }
 
 eraser.addEventListener('click', () => {
     eraserMode = true;
-    if (eraserMode) {
-        eraser.classList.add('active');
-    }
+    rainbowMode = false;
+    eraser.classList.add('active');
+    colorBtn.classList.remove('active');
+    rainbow.classList.remove('active');
 })
 
-for (let i = 0; i < currentSize * currentSize; i++) {
-    const gridCell = document.createElement('div');
-    gridCell.classList.add('grid-cell');
-    gridCell.addEventListener('mouseover', () => {
-        if (!eraserMode) {
-            gridCell.style.backgroundColor = currentColor;
-        }
-        else {
-            gridCell.style.backgroundColor = eraserColor;
-        }
-    })
-    grid.appendChild(gridCell);
+colorBtn.addEventListener('click', () => {
+    eraserMode = false;
+    rainbowMode = false;
+    colorBtn.classList.add('active');
+    eraser.classList.remove('active');
+    rainbow.classList.remove('active');
+})
+
+rainbow.addEventListener('click', () => {
+    rainbowMode = true;
+    eraserMode = false;
+    rainbow.classList.add('active');
+    colorBtn.classList.remove('active');
+    eraser.classList.remove('active');
+})
+
+clear.addEventListener('click', () => {
+    const gridCells = document.querySelectorAll('.grid-cell');
+    gridCells.forEach(cell => {
+        cell.style.backgroundColor = eraserColor;
+    });
+})
+
+slider.addEventListener('input', (e) => {
+    const newSize = parseInt(e.target.value);
+    setSize(newSize);
+    createGrid(newSize);
+});
+
+function createGrid(size) {
+    grid.innerHTML = '';
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < currentSize * currentSize; i++) {
+        colorBtn.classList.add('active');
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        gridCell.addEventListener('mouseover', () => {
+            if (!eraserMode && !rainbowMode) {
+                gridCell.style.backgroundColor = currentColor;
+            }
+            else if (rainbowMode && !eraserMode) {
+                const r = Math.floor(Math.random() * 256)
+                const g = Math.floor(Math.random() * 256)
+                const b = Math.floor(Math.random() * 256)
+                gridCell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            }
+            else {
+                gridCell.style.backgroundColor = eraserColor;
+            }
+        })
+        grid.appendChild(gridCell);
+    }
 }
+
+createGrid(currentSize);
 
 color.oninput = (e) => setColor(e.target.value);
